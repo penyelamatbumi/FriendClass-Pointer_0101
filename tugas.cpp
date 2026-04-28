b8185e4d7372e4d50b823bd1595d784930d5cbad
penyelamatbumi <inihanip7@gmail.com>
2026-04-28 19:34:05 +0700

    buat fungsi input data interaktif untuk layang"

--- tugas.cpp
+++ tugas.cpp
@@ -9,3 +9,13 @@ class LayangLayang {
 private:
     double d1, d2, sisiA, sisiB;
 
+public:
+    void inputData() {
+        cout << "[ Input Layang-Layang ]" << endl;
+        cout << "Masukkan Diagonal 1 : "; cin >> d1;
+        cout << "Masukkan Diagonal 2 : "; cin >> d2;
+        cout << "Masukkan Sisi Miring Atas : "; cin >> sisiA;
+        cout << "Masukkan Sisi Miring Bawah: "; cin >> sisiB;
+    }
+
+   
