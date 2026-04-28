732e9ba6ddf89189c41e734f4e2dae439957acc0
penyelamatbumi <inihanip7@gmail.com>
2026-04-28 19:38:13 +0700

    membuat fungsi input data untuk belah ketupat

--- tugas.cpp
+++ tugas.cpp
@@ -38,3 +38,12 @@ class BelahKetupat {
 private:
     double d1, d2, sisi;
 
+public:
+    void inputData() {
+        cout << "\n[ Input Belah Ketupat ]" << endl;
+        cout << "Masukkan Diagonal 1 : "; cin >> d1;
+        cout << "Masukkan Diagonal 2 : "; cin >> d2;
+        cout << "Masukkan Panjang Sisi: "; cin >> sisi;
+    }
+
+   
